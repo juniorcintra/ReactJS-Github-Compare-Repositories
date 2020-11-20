b7a74aa08d631dfa99438adac309f93ca2a30ba2
juniorcintra <junior.developer@outlook.com.br>
2020-11-20 03:28:55 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Main from './pages/Main';
+
+import GlobalStyle from './styles/global';
+
+export default function App(){
+    return (
+        <>
+        <GlobalStyle />
+        <Main />
+        </>
+    )
+}
